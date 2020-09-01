c749c9c5c700507d4619e3153f6e0e2a1d744ec6
Anakaren <ak.rojas.07@gmail.com>
2020-09-01 10:40:34 -0700

    Removed reference to domain project

---
+++ UserManagement/User.Infrastructure/Repository/Entities/UserAccount.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace User.Infrastructure.Repository.Entities
+{
+    public partial class UserAccount : DbContext
+    {
+        public UserAccount() { }
+
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public bool Status { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
