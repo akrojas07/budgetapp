3cc2262dc36457143c24110e3cb763577cda7c5e
Anakaren <ak.rojas.07@gmail.com>
2020-09-01 10:37:36 -0700

    User Service methods

---
+++ UserManagement/User.Domain/Services/Interfaces/IUserServices.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using User.Domain.Models;
+
+namespace User.Domain.Services.Interfaces
+{
+    public interface IUserServices
+    {
+        // ------ Create New User Account ----- \\
+        Task CreateNewUserAccount(CoreUser coreUser);
+
+
+        // --------- pull user object ----------\\ 
+        Task<CoreUser> GetUserByEmail(string email);
+
+
+        // --------- update user --------------- \\ remember to update the "updated" date property
+        Task UpdateUserEmail(long userId, string email);
+        Task UpdateUserPassword(long userId, string password);
+        Task UpdateName(long userId, string nameType, string name);
+        Task LogIn(long userId, string password);
+        Task LogOut(long userId);
+        
+
+
+        //--------- delete user ----------------- \\
+        Task DeleteUserAccount(long userId);
+    }
+}
---
+++ UserManagement/User.Domain/Services/UserServices.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using User.Domain.Models;
+using User.Domain.Services.Interfaces;
+using User.Infrastructure.Repository.Interfaces;
+using User.Domain.DbMapper;
+
+namespace User.Domain.Services
+{
+    public class UserServices : IUserServices
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserServices(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        /// <summary>
+        /// Method to create new user account
+        /// Validates user does not exist based on email address 
+        /// </summary>
+        /// <param name="coreUser"></param>
+        /// <returns>Task</returns>
+        public async Task CreateNewUserAccount(CoreUser coreUser)
+        {
+            //pull any user that exists with email address provided
+            var existingUser = await _userRepository.GetUserByEmail(coreUser.Email);
+            
+            //validate that user is null
+            if(existingUser != null)
+            {
+                throw new Exception("User with associated email exists.");
+            }
+
+            //map from core to db user
+            var dbUser = EfUserMapper.CoreToDbUser(coreUser);
+            
+            //create new user account with repository method
+            await _userRepository.CreateNewUserAccount(dbUser);
+
+        }
+
+        /// <summary>
+        /// Method to delete user account by User Id
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>Task</returns>
+        public async Task DeleteUserAccount(long userId)
+        {
+            //pull user object
+            var existingUser = _userRepository.GetUserByUserId(userId);
+            
+            //validate user exists
+            if(existingUser == null)
+            {
+                throw new Exception("User does not exist");
+            }
+
+            await _userRepository.DeleteUserAccount(userId);
+        }
+
+        /// <summary>
+        /// Method to pull user object by email
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>Task</returns>
+        public async Task<CoreUser> GetUserByEmail(string email)
+        {
+            //pull user object
+            var dbUser = await _userRepository.GetUserByEmail(email);
+
+            //validate user exists
+            if(dbUser == null)
+            {
+                throw new Exception("User does not exist");
+            }
+
+            //map db user to core user
+            CoreUser coreUser = EfUserMapper.DbToCoreUser(dbUser);
+
+            return coreUser;
+
+        }
+        /// <summary>
+        /// Method to log user in
+        /// Validates password
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="password"></param>
+        /// <returns>Task</returns>
+
+        public async Task LogIn(long userId, string password)
+        {
+            //pull user object
+            var user = await _userRepository.GetUserByUserId(userId);
+
+            //validate user exists
+            if(user == null)
+            {
+                throw new Exception("User does not exist");
+            }
+
+            //validate password is correct
+            while(user.Password != password)
+            {
+                throw new Exception("Invalid password");
+            }
+
+            //log user in
+            await _userRepository.UpdateStatus(userId, true);
+
+        }
+        /// <summary>
+        /// Method to log user out
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public async Task LogOut(long userId)
+        {
+            //pull user object
+            var user = await _userRepository.GetUserByUserId(userId);
+
+            //validate user exists
+            if (user == null)
+            {
+                throw new Exception("User does not exist");
+            }
+
+            //log user out
+            await _userRepository.UpdateStatus(userId, false);
+        }
+
+        /// <summary>
+        /// Method to update user's name
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="nameType">Refers to either first name or last name</param>
+        /// <param name="name"></param>
+        /// <returns>Task</returns>
+        public async Task UpdateName(long userId, string nameType, string name)
+        {
+            //pull user object
+            var user = await _userRepository.GetUserByUserId(userId);
+
+            //validate user exists
+            if(user == null)
+            {
+                throw new Exception("User does not exist");
+            }
+
+            await _userRepository.UpdateName(userId, nameType, name);
+
+        }
+
+        /// <summary>
+        /// Method to update user email
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="email"></param>
+        /// <returns>Task</returns>
+        public async Task UpdateUserEmail(long userId, string email)
+        {
+            var user = await _userRepository.GetUserByUserId(userId);
+
+            if(user == null)
+            {
+                throw new Exception("User does not exist");
+            }
+
+            await _userRepository.UpdateUserEmail(userId, email);
+        }
+        /// <summary>
+        /// Method to update user password
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="password"></param>
+        /// <returns>Task</returns>
+
+        public async Task UpdateUserPassword(long userId, string password)
+        {
+            var user = await _userRepository.GetUserByUserId(userId);
+
+            if(user == null)
+            {
+                throw new Exception("User does not exist");
+            }
+
+            await _userRepository.UpdateUserPassword(userId, password);
+        }
+    }
+}
