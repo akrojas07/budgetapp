abf7d391473479df2da4b560869aef6640ace96a
Anakaren <ak.rojas.07@gmail.com>
2020-10-13 13:37:24 -0700

    added references and injected repos, services

--- BudgetManagement/BudgetManagement.API/Startup.cs
+++ BudgetManagement/BudgetManagement.API/Startup.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BudgetManagement.Persistence.Repositories;
+using BudgetManagement.Persistence.Repositories.Interfaces;
+using BudgetManagement.Domain.Services.Interfaces;
+using BudgetManagement.Domain.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +20,16 @@ namespace BudgetManagement.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IBudgetExpensesRepository, BudgetExpensesRepository>();
+            services.AddSingleton<IBudgetIncomeRepository, BudgetIncomeRepository>();
+            services.AddSingleton<IBudgetSavingsRepository, BudgetSavingRepository>();
+
+
+            services.AddTransient<IBudgetIncomeServices, BudgetIncomeServices>();
+            services.AddTransient<IBudgetExpensesServices, BudgetExpensesServices>();
+            services.AddTransient<IBudgetSavingsServices, BudgetSavingsServices>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +44,7 @@ namespace BudgetManagement.API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
